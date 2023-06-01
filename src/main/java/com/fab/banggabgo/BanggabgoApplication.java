4449b86f431329cd012dd69a652046db0ac03ea4
yjjjwww <yjjjwww@naver.com>
2023-06-02 04:57:06 +0900

    Initial commit

---
+++ src/main/java/com/fab/banggabgo/BanggabgoApplication.java
@@ -0,0 +1,13 @@
+package com.fab.banggabgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanggabgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanggabgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fab/banggabgo/BanggabgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fab.banggabgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanggabgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

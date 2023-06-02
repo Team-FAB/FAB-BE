bf6243c624e3bd821a9fca365afbf4ed32cdd5e4
sjdlr <sjdlrnen0507@naver.com>
2023-06-03 00:21:29 +0900

    [feat] Create enum
    공통적으로 사용하는 enum

---
+++ src/main/java/com/fab/banggabgo/type/Gender.java
@@ -0,0 +1,5 @@
+package com.fab.banggabgo.type;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/fab/banggabgo/type/Mbti.java
@@ -0,0 +1,5 @@
+package com.fab.banggabgo.type;
+
+public enum Mbti {
+    INFP, ENFP, INFJ, ENFJ, INTJ, ENTJ, INTP, ENTP, ISFP, ESFP, ISTP, ESTP, ISFJ, ESFJ, ISTJ, ESTJ
+}

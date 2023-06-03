dc2a40f715581b9d7e5af7bed34c7f6cec10545d
FeelingXD <wlals425315@gmail.com>
2023-06-03 15:15:00 +0900

    add : default user entity
    - 기본적인 Spring security 를 상속받은 user entity 구현

---
+++ src/main/java/com/fab/banggabgo/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.fab.banggabgo.repository;
+
+import com.fab.banggabgo.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+  @Query("select u from User u left join fetch u.roles where u.email=:email")
+  Optional<User> findByEmail(String email);
+
+  Integer countByEmail(String email);
+  Integer countByName(String name);
+
+}

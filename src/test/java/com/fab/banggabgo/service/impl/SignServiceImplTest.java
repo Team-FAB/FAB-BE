ff04c8227dbf2a125f77a1b262a016656c032a2d
FeelingXD <wlals425315@gmail.com>
2023-06-04 19:47:39 +0900

    test : 로그아웃기능 테스트 추가 기존 로직변경

--- src/test/java/com/fab/banggabgo/service/impl/SignServiceImplTest.java
+++ src/test/java/com/fab/banggabgo/service/impl/SignServiceImplTest.java
@@ -2,6 +2,9 @@ package com.fab.banggabgo.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,6 +19,8 @@ import com.fab.banggabgo.entity.User;
 import com.fab.banggabgo.repository.UserRepository;
 import com.fab.banggabgo.service.SignService;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,6 +28,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
@@ -36,8 +45,18 @@ class SignServiceImplTest {
   @Mock
   JwtTokenProvider jwtTokenProvider;
 
+  @Mock
+  RedisTemplate<String,String> redisTemplate;
+  @Mock
+  ValueOperations<String,String> valueOperations;
+  @Mock
+  HttpServletRequest request;
+
+  @Mock
+  UsernamePasswordAuthenticationToken user;
   @InjectMocks
   SignServiceImpl signService;
+
   @Nested
   @DisplayName("SignUp - 회원가입")
   class SignUp{
@@ -59,8 +78,8 @@ class SignServiceImplTest {
       when(userRepository.save(any(User.class))).thenReturn(user);
 
 
-      signService.signUp(requestDto);
         //then
+      signService.signUp(requestDto);
       verify(userRepository,times(1)).save(any());
     }
     @DisplayName("회원가입- 이미 이메일이 존재할경우")
@@ -114,10 +133,15 @@ class SignServiceImplTest {
       when(passwordEncoder.matches(any(), any())).thenReturn(true);when(passwordEncoder.matches(any(), any())).thenReturn(true);
       when(jwtTokenProvider.createAccessToken(any(),any())).thenReturn("accesstoken");
       when(jwtTokenProvider.createRefreshToken()).thenReturn("rtktoken");
-
+      when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+      var result= signService.signIn(dto);
 
       //then
-      var result= signService.signIn(dto);
+      verify(userRepository, times(1)).findByEmail(email);
+      verify(passwordEncoder, times(1)).matches(password, user.getPassword());
+      verify(jwtTokenProvider, times(1)).createAccessToken(user.getEmail(), user.getRoles());
+      verify(jwtTokenProvider, times(1)).createRefreshToken();
+      verify(jwtTokenProvider, times(1)).getExpiration(anyString());
       assertEquals(result.getToken().getAtk(),stub_result.getToken().getAtk());
       assertEquals(result.getToken().getRtk(),stub_result.getToken().getRtk());
     }
@@ -136,6 +160,43 @@ class SignServiceImplTest {
       //then
       assertThrows(RuntimeException.class,() -> signService.signIn(dto));
     }
+  }
+  @Nested
+  @DisplayName(" Logout- 로그아웃")
+  class Logout{
+    String stub_atk= "stub_atk";
+
+    @Test
+    @DisplayName("Logout - 만료된토큰")
+    void logout_expiredATk(){ // 만료된 토큰일경우
+        //given
+        //when
+      when(jwtTokenProvider.resolveAtk(any())).thenReturn(stub_atk);
+      when(jwtTokenProvider.validateToken(stub_atk)).thenReturn(false);
+
+        //then
+      assertThrows(RuntimeException.class,() -> signService.logout(any()));
+    }
+    @Test
+    @DisplayName("Logout - 정상 로그아웃")
+    void logout(){
+        //given
+      long expiration = 3600000; // 1 hour
+
+        //when
+      when(jwtTokenProvider.resolveAtk(any(HttpServletRequest.class))).thenReturn(stub_atk);
+      when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+      when(jwtTokenProvider.validateToken(stub_atk)).thenReturn(true);
+      when(redisTemplate.opsForValue().get(anyString())).thenReturn(stub_atk);
+      when(jwtTokenProvider.getExpiration(stub_atk)).thenReturn(expiration);
+      when(jwtTokenProvider.getAuthentication(stub_atk)).thenReturn(user);
+      when(user.getName()).thenReturn("test");
+      //then
+      signService.logout(request);
+      verify(redisTemplate.opsForValue(),times(1)).set(anyString(),eq("logout"),eq(expiration),eq(TimeUnit.MILLISECONDS) );
+
+    }
+
   }
 
 }

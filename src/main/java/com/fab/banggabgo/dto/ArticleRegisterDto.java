9922afedaf4fa5cc37724dd34f234cd430a96993
yjjjwww <yjjjwww@naver.com>
2023-06-05 14:00:56 +0900

    feat : User Article 등록하기 기능 구현
    - JWT 토큰으로 User 검증
    - 글 등록 양식이 올바른지 검증
    - Gender, Seoul 값 검증

---
+++ src/main/java/com/fab/banggabgo/dto/ArticleRegisterDto.java
@@ -0,0 +1,22 @@
+package com.fab.banggabgo.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleRegisterDto {
+  private String title;
+  private String region;
+  private LocalDate period;
+  private Integer price;
+  private String gender;
+  private String content;
+}
---
+++ src/main/java/com/fab/banggabgo/dto/ArticleRegisterForm.java
@@ -0,0 +1,35 @@
+package com.fab.banggabgo.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleRegisterForm {
+
+  private String title;
+  private String region;
+  private LocalDate period;
+  private Integer price;
+  private String gender;
+  private String content;
+  public static ArticleRegisterDto toDto(ArticleRegisterForm form){
+    return ArticleRegisterDto.builder()
+        .title(form.getTitle())
+        .region(form.getRegion())
+        .period(form.getPeriod())
+        .price(form.getPrice())
+        .gender(form.getGender())
+        .content(form.getContent())
+        .build();
+  }
+}
+
+

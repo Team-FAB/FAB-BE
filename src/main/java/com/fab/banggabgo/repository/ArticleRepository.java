a156b8182f431acc48855de8a93ba6a5399995e5
yjjjwww <yjjjwww@naver.com>
2023-06-07 03:06:13 +0900

    fix : ArticleServiceImpl isRecruiting 관련, isStringEmpty 관련 코드 수정
    - 작성 게시글 개수 가져오는 방식에 isRecruiting 상태도 체크 추가
    - 글 작성시 isRecruiting 기본값은 true로 변경
    - 빈 문자열 체크를 StringUtils.hasText 를 사용하는 것으로 변경

--- src/main/java/com/fab/banggabgo/repository/ArticleRepository.java
+++ src/main/java/com/fab/banggabgo/repository/ArticleRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long>{
 
-  int countByUserAndIsDeletedFalse(User user);
+  int countByUserAndIsDeletedFalseAndIsRecruitingTrue(User user);
 }

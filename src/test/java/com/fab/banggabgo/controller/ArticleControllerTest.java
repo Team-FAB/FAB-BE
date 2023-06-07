48d9348aeee0cd1cac0d173b12a0cc9d397f0f16
yjjjwww <yjjjwww@naver.com>
2023-06-08 00:36:41 +0900

    HotFix : Test @SpringBootTest 대신 @WebMvcTest 사용
    - 테스트 코드 @WithMockUser, .with(SecurityMockMvcRequestPostProcessors.csrf())를 사용하여 인증 관리
    - test/resources 삭제

--- src/test/java/com/fab/banggabgo/controller/ArticleControllerTest.java
+++ src/test/java/com/fab/banggabgo/controller/ArticleControllerTest.java
@@ -17,16 +17,21 @@ import com.fab.banggabgo.service.ArticleService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ArticleController.class)
+@ExtendWith(MockitoExtension.class)
 class ArticleControllerTest {
 
   @MockBean
@@ -40,6 +45,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 등록 성공")
+  @WithMockUser
   void postArticleSuccess() throws Exception {
     //given
     ArticleRegisterForm form = ArticleRegisterForm.builder()
@@ -54,6 +60,7 @@ class ArticleControllerTest {
     //when
     //then
     mockMvc.perform(post("/api/article")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -63,6 +70,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 등록 실패 : 글 양식 오류")
+  @WithMockUser
   void postArticleFail_INVALID_REGISTER() throws Exception {
     //given
     ArticleRegisterForm form = ArticleRegisterForm.builder()
@@ -80,6 +88,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(post("/api/article")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -93,6 +102,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 등록 실패 : 지역 오류")
+  @WithMockUser
   void postArticleFail_INVALID_REGION() throws Exception {
     //given
     ArticleRegisterForm form = ArticleRegisterForm.builder()
@@ -110,6 +120,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(post("/api/article")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -123,6 +134,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 등록 실패 : 성별 오류")
+  @WithMockUser
   void postArticleFail_INVALID_GENDER() throws Exception {
     //given
     ArticleRegisterForm form = ArticleRegisterForm.builder()
@@ -140,6 +152,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(post("/api/article")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -153,6 +166,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 수정 성공")
+  @WithMockUser
   void putArticleSuccess() throws Exception {
     //given
     ArticleEditForm form = ArticleEditForm.builder()
@@ -167,6 +181,7 @@ class ArticleControllerTest {
     //when
     //then
     mockMvc.perform(put("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -176,6 +191,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 수정 실패 : 글 양식 오류")
+  @WithMockUser
   void putArticleFail_INVALID_EDIT() throws Exception {
     //given
     ArticleEditForm form = ArticleEditForm.builder()
@@ -193,6 +209,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(put("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -206,6 +223,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 수정 실패 : 글을 찾을 수 없음")
+  @WithMockUser
   void putArticleFail_NOT_FOUND_ARTICLE() throws Exception {
     //given
     ArticleEditForm form = ArticleEditForm.builder()
@@ -223,6 +241,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(put("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -236,6 +255,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 수정 실패 : 삭제된 게시글은 수정 불가")
+  @WithMockUser
   void putArticleFail_DELETED_ARTICLE() throws Exception {
     //given
     ArticleEditForm form = ArticleEditForm.builder()
@@ -253,6 +273,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(put("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -266,6 +287,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 수정 실패 : 해당 게시글의 작성자만 수정 가능")
+  @WithMockUser
   void putArticleFail_INVALID_USER() throws Exception {
     //given
     ArticleEditForm form = ArticleEditForm.builder()
@@ -283,6 +305,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(put("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(form)))
@@ -296,11 +319,13 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 삭제 성공")
+  @WithMockUser
   void deleteArticleSuccess() throws Exception {
     //given
     //when
     //then
     mockMvc.perform(delete("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT"))
         .andExpect(status().isNoContent())
         .andDo(print());
@@ -308,6 +333,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 삭제 실패 : 게시글 찾을 수 없음")
+  @WithMockUser
   void deleteArticleFail_NOT_FOUND_ARTICLE() throws Exception {
     //given
     doThrow(new RuntimeException("해당 게시글을 찾을 수 없습니다."))
@@ -316,6 +342,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(delete("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT"))
         .andExpect(status().isInternalServerError())
         .andReturn();
@@ -327,6 +354,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 삭제 실패 : 이미 삭제된 게시글")
+  @WithMockUser
   void deleteArticleFail_DELETED_ARTICLE() throws Exception {
     //given
     doThrow(new RuntimeException("해당 게시글을 찾을 수 없습니다."))
@@ -335,6 +363,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(delete("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT"))
         .andExpect(status().isInternalServerError())
         .andReturn();
@@ -346,6 +375,7 @@ class ArticleControllerTest {
 
   @Test
   @DisplayName("글 삭제 실패 : 해당 게시글의 작성자만 삭제 가능")
+  @WithMockUser
   void deleteArticleFail_INVALID_USER() throws Exception {
     //given
     doThrow(new RuntimeException("해당 게시글의 작성자가 아닙니다."))
@@ -354,6 +384,7 @@ class ArticleControllerTest {
 
     //when
     MvcResult result = mockMvc.perform(delete("/api/article/1")
+            .with(SecurityMockMvcRequestPostProcessors.csrf())
             .header("Authorization", "JWT"))
         .andExpect(status().isInternalServerError())
         .andReturn();

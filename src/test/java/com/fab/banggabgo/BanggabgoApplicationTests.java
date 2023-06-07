8e37b45f431b462c01a9d9e1646af25ed3a64a42
yjjjwww <yjjjwww@naver.com>
2023-06-07 23:11:10 +0900

    fix : Test 환경 변경

--- src/test/java/com/fab/banggabgo/BanggabgoApplicationTests.java
+++ src/test/java/com/fab/banggabgo/BanggabgoApplicationTests.java
@@ -3,7 +3,7 @@ package com.fab.banggabgo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class BanggabgoApplicationTests {
 
 	@Test

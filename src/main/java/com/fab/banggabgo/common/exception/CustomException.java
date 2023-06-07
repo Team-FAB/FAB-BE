54ccc8633c513f2168aba6d8650b839ccd481d92
FeelingXD <wlals425315@gmail.com>
2023-06-07 19:08:52 +0900

    feat: CustomException & ApiResponse

---
+++ src/main/java/com/fab/banggabgo/common/exception/CustomException.java
@@ -0,0 +1,13 @@
+package com.fab.banggabgo.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+  private final ErrorCode errorCode;
+
+  public CustomException(ErrorCode e){
+    super(e.getMsg());
+    this.errorCode=e;
+  }
+}

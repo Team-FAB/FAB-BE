6a7074058729a0a5571bbdbaccc045ac26a8ea36
yjjjwww <yjjjwww@naver.com>
2023-06-08 21:47:30 +0900

    feat : 게시글 최신순으로 조회하기 기능 구현
    - Querydsl 설정 및 Repository 추가
    - isRecruiting 값이 true이면 모집중인 글만, false이면 전체 글 조회, 페이징 처리
    - 조회할 때 isDeleted 값이 false인 Article만 조회
    - 수정된 ApiResponse ArticleController에 적용

--- src/main/java/com/fab/banggabgo/controller/ArticleController.java
+++ src/main/java/com/fab/banggabgo/controller/ArticleController.java
@@ -1,18 +1,23 @@
 package com.fab.banggabgo.controller;
 
+import com.fab.banggabgo.common.ApiResponse;
+import com.fab.banggabgo.common.ResponseCode;
 import com.fab.banggabgo.dto.ArticleEditForm;
 import com.fab.banggabgo.dto.ArticleRegisterForm;
 import com.fab.banggabgo.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,7 +33,7 @@ public class ArticleController {
       @RequestBody ArticleRegisterForm form
   ) {
     articleService.postArticle(token, ArticleRegisterForm.toDto(form));
-    return ResponseEntity.created(null).build();
+    return ApiResponse.builder().code(ResponseCode.RESPONSE_CREATED).toEntity();
   }
 
   @PutMapping("/{id}")
@@ -38,7 +43,7 @@ public class ArticleController {
       @RequestBody ArticleEditForm form
   ) {
     articleService.putArticle(token, id, ArticleEditForm.toDto(form));
-    return ResponseEntity.created(null).build();
+    return ApiResponse.builder().code(ResponseCode.RESPONSE_CREATED).toEntity();
   }
 
   @DeleteMapping("/{id}")
@@ -47,6 +52,16 @@ public class ArticleController {
       @PathVariable long id
   ) {
     articleService.deleteArticle(token, id);
-    return ResponseEntity.noContent().build();
+    return ApiResponse.builder().code(ResponseCode.RESPONSE_SUCCESS).toEntity();
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getArticleByPageable(
+      @RequestParam("page") Integer page,
+      @RequestParam("size") Integer size,
+      @RequestParam(value = "isRecruiting", defaultValue = "true") boolean isRecruiting
+  ) {
+    var result = articleService.getArticleByPageable(page, size, isRecruiting);
+    return ApiResponse.builder().code(ResponseCode.RESPONSE_SUCCESS).data(result).toEntity();
   }
 }

9ded6f63f669c8797a6d1d4b5ba121afb33b86df
FeelingXD <wlals425315@gmail.com>
2023-06-09 17:00:09 +0900

    docs: swagger 설명분 추가
    - service 작업에대한 설명 추가
    - form 에대한 설명추가
    - controller 에대한 설명 추가

--- src/main/java/com/fab/banggabgo/dto/SignUpRequestForm.java
+++ src/main/java/com/fab/banggabgo/dto/SignUpRequestForm.java
@@ -1,5 +1,6 @@
 package com.fab.banggabgo.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,14 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SignUpRequestForm {
 
+  @ApiModelProperty(value = "이메일",example = "test@test.com")
+  
   private String email;
+  @ApiModelProperty(value = "비밀번호",example = "MyPassword@123")
+  
   private String password;
+  @ApiModelProperty(value = "닉네임",example = "수줍은라이언")
+  
   private String nickname;
   public static SignUpRequestDto toDto(SignUpRequestForm form){
     return SignUpRequestDto.builder()

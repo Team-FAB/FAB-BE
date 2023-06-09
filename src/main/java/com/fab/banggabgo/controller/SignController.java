2bd540639d68dcc94eb21f9f440d189d640162b8
FeelingXD <wlals425315@gmail.com>
2023-06-09 17:00:09 +0900

    docs: swagger 설명분 추가
    - service 작업에대한 설명 추가
    - form 에대한 설명추가
    - controller 에대한 설명 추가

--- src/main/java/com/fab/banggabgo/controller/SignController.java
+++ src/main/java/com/fab/banggabgo/controller/SignController.java
@@ -5,6 +5,8 @@ import com.fab.banggabgo.common.ResponseCode;
 import com.fab.banggabgo.dto.SignInRequestForm;
 import com.fab.banggabgo.dto.SignUpRequestForm;
 import com.fab.banggabgo.service.SignService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"Sign Controller 로그인 API"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
@@ -24,31 +27,49 @@ public class SignController {
 
   private final SignService signService;
 
+  @ApiOperation(
+      value = "사용자 회원가입",
+      notes = "사용자에게 입력을 받고 회원가입 합니다."
+  )
   @PostMapping("/register")
   public ResponseEntity<?> signUp(@RequestBody SignUpRequestForm form){
     signService.signUp(SignUpRequestForm.toDto(form));
     return ApiResponse.builder().code(ResponseCode.RESPONSE_CREATED).toEntity();
   }
-  @PermitAll
+  @ApiOperation(
+      value = "가입 이메일 체크",
+      notes = "사용자로부터 이메일을 입력 받고 이미 가입된 이메일 인지 체크"
+  )
   @GetMapping("/register/email-check")
   public ResponseEntity<?> emailCheck(@RequestParam String email){
     var result= signService.emailCheck(email);
     return ApiResponse.builder().code(ResponseCode.RESPONSE_SUCCESS).data(result).toEntity();
 
   }
-  @PermitAll
+  @ApiOperation(
+      value = "가입 닉네임 체크 ",
+      notes = "사용자로부터 닉네임을 입력 받고 이미 가입된 닉네임 인지 체크"
+  )
   @GetMapping("/register/nickname-check")
   public ResponseEntity<?> nickNameCheck(@RequestParam String nickname){
     var result= signService.nickNameCheck(nickname);
     return ApiResponse.builder().code(ResponseCode.RESPONSE_SUCCESS).data(result).toEntity();
   }
 
+  @ApiOperation(
+      value = "로그인",
+      notes = "사용자로부터 아이디와 패스워드를 입력 받고 로그인에 대한 응답값 반환"
+  )
   @PostMapping("/login")
   public ResponseEntity<?> signIn(@RequestBody SignInRequestForm form){
     var result = signService.signIn(SignInRequestForm.toDto(form));
     return ApiResponse.builder().code(ResponseCode.RESPONSE_SUCCESS).data(result).toEntity();
   }
 
+  @ApiOperation(
+      value = "로그아웃",
+      notes = "로그인된 사용자를 로그아웃 처리함"
+  )
   @PostMapping("/logout")
   public ResponseEntity<?> logout(HttpServletRequest req){
     var result = signService.logout(req);
--- src/main/java/com/fab/banggabgo/dto/SignInRequestForm.java
+++ src/main/java/com/fab/banggabgo/dto/SignInRequestForm.java
@@ -1,5 +1,6 @@
 package com.fab.banggabgo.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequestForm {
+  @ApiModelProperty(value = "이메일",example = "test@test.com")
   private String email;
+  @ApiModelProperty(value = "비밀번호",example = "MyPassword@123")
   private String password;
 
   public static SignInRequestDto toDto(SignInRequestForm form) {

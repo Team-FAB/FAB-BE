b5962b6b2b9fdba594b6186f811bff735151a99e
FeelingXD <wlals425315@gmail.com>
2023-06-11 19:22:06 +0900

    chore : @AthenticationPriciple 에대한 Swagger require 무시

--- src/main/java/com/fab/banggabgo/config/SwaggerConfig.java
+++ src/main/java/com/fab/banggabgo/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -25,6 +26,7 @@ public class SwaggerConfig {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.OAS_30)
+        .ignoredParameterTypes(AuthenticationPrincipal.class)
         .useDefaultResponseMessages(false)
         .select()
         .apis(RequestHandlerSelectors.any())

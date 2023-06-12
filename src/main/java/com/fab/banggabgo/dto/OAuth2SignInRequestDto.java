6c6c06ae28df2254ecff20b91015f86e791a0ca5
sjdlr <sjdlrnen0507@naver.com>
2023-06-12 18:20:52 +0900

    feat: OAuth2 로그인을 위한 requestDto 구현

---
+++ src/main/java/com/fab/banggabgo/dto/OAuth2SignInRequestDto.java
@@ -0,0 +1,16 @@
+package com.fab.banggabgo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OAuth2SignInRequestDto {
+  String accessToken;
+}
---
+++ src/main/java/com/fab/banggabgo/dto/OAuth2SignInRequestForm.java
@@ -0,0 +1,8 @@
+package com.fab.banggabgo.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class OAuth2SignInRequestForm {
+@ApiModelProperty(value = "access_token")
+  String accessToken;
+}

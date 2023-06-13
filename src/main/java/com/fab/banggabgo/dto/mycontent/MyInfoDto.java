aa007b953ffa7a4639753715161d6bb5cfb5fd05
지민 <wlals425315@gmail.com>
2023-06-13 23:36:45 +0900

    fix : 정책변경에따른 로직추가
    - activity time enum의 value를 주고받도록 수정

--- src/main/java/com/fab/banggabgo/dto/mycontent/MyInfoDto.java
+++ src/main/java/com/fab/banggabgo/dto/mycontent/MyInfoDto.java
@@ -3,7 +3,6 @@ package com.fab.banggabgo.dto.mycontent;
 import com.fab.banggabgo.entity.User;
 import com.fab.banggabgo.type.ActivityTime;
 import com.fab.banggabgo.type.Gender;
-import com.fab.banggabgo.type.Mbti;
 import com.fab.banggabgo.type.Seoul;
 import java.util.HashSet;
 import java.util.Optional;
@@ -31,10 +30,7 @@ public class MyInfoDto {
   private String gender;
   private String region;
   private String mbti;
-  private Set<String> tags;
-  private int minAge;
-  private int maxAge;
-
+  private Set<String> tags = new HashSet<>();
   private String preferredAge;
   private int myAge;
   private String detail;
@@ -48,10 +44,8 @@ public class MyInfoDto {
         .activityTime(Optional.ofNullable(user.getActivityTime()).map(ActivityTime::getValue).orElse("null"))
         .gender(Optional.ofNullable(user.getGender()).map(Gender::getValue).orElse("null"))
         .region(Optional.ofNullable(user.getRegion()).map(Seoul::getValue).orElse("null"))
-        .mbti(Optional.ofNullable(user.getMbti()).map(Mbti::name).orElse("null"))
         .tags(user.getTag())
-        .minAge(user.getMinAge())
-        .maxAge(user.getMaxAge())
+        .preferredAge(Optional.ofNullable(user.getMbti()).map(Enum::name).orElse("null"))
         .myAge(user.getMyAge())
         .detail(user.getDetail())
         .build();

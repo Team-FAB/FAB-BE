8519e0f703bcbf149f29560be208b2c5ba942ccb
sjdlr <sjdlrnen0507@naver.com>
2023-06-15 00:49:01 +0900

    fix: User int형을 null이 가능한 Integer로 변환

--- src/main/java/com/fab/banggabgo/dto/mycontent/MyInfoDto.java
+++ src/main/java/com/fab/banggabgo/dto/mycontent/MyInfoDto.java
@@ -3,6 +3,7 @@ package com.fab.banggabgo.dto.mycontent;
 import com.fab.banggabgo.entity.User;
 import com.fab.banggabgo.type.ActivityTime;
 import com.fab.banggabgo.type.Gender;
+import com.fab.banggabgo.type.Mbti;
 import com.fab.banggabgo.type.Seoul;
 import java.util.HashSet;
 import java.util.Optional;
@@ -30,7 +31,7 @@ public class MyInfoDto {
   private String gender;
   private String region;
   private String mbti;
-  private Set<String> tags = new HashSet<>();
+  private Set<String> tags;
   private int minAge;
   private int maxAge;
 
@@ -47,8 +48,10 @@ public class MyInfoDto {
         .activityTime(Optional.ofNullable(user.getActivityTime()).map(ActivityTime::getValue).orElse("null"))
         .gender(Optional.ofNullable(user.getGender()).map(Gender::getValue).orElse("null"))
         .region(Optional.ofNullable(user.getRegion()).map(Seoul::getValue).orElse("null"))
+        .mbti(Optional.ofNullable(user.getMbti()).map(Mbti::name).orElse("null"))
         .tags(user.getTag())
-        .preferredAge(Optional.ofNullable(user.getMbti()).map(Enum::name).orElse("null"))
+        .minAge(user.getMinAge())
+        .maxAge(user.getMaxAge())
         .myAge(user.getMyAge())
         .detail(user.getDetail())
         .build();

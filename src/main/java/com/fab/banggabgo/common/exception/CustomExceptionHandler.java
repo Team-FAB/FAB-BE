a6623a293c0bed5d7890295d62cfae1b5a808636
yjjjwww <yjjjwww@naver.com>
2023-06-16 23:28:40 +0900

    feat : AuthenticationException ExceptionHandler 추가

--- src/main/java/com/fab/banggabgo/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/fab/banggabgo/common/exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fab.banggabgo.common.exception;
 
 import com.fab.banggabgo.common.ApiResponse;
+import javax.security.sasl.AuthenticationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,5 +14,10 @@ public class CustomExceptionHandler {
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<?> customExceptionResponseEntity(CustomException e) {
     return ApiResponse.builder().code(e.getErrorCode()).toEntity();
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<?> authenticationExceptionResponseEntity(AuthenticationException e) {
+    return ApiResponse.builder().code(ErrorCode.USER_IS_NULL).toEntity();
   }
 }

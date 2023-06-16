7610325bc9cb24422cf6197636b8a2cd029e3951
yjjjwww <yjjjwww@naver.com>
2023-06-16 22:37:47 +0900

    feat : 프로필 추천 기능 구현
    - user와 size를 받으면 해당 user와 성별, 흡연유무, 지역, 활동시간, 원하는 연령대가 같은 user 목록을 랜덤으로 size만큼 가져옴
    - 응답으로 로그인한 유저의 mbti와 추천 유저들의 id, 닉네임, mbti 정보를 보냄

--- src/main/java/com/fab/banggabgo/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/fab/banggabgo/repository/impl/UserRepositoryImpl.java
@@ -3,7 +3,9 @@ package com.fab.banggabgo.repository.impl;
 import com.fab.banggabgo.entity.QUser;
 import com.fab.banggabgo.entity.User;
 import com.fab.banggabgo.repository.UserRepositoryCustom;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +20,23 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
             .leftJoin(qUser.tag).fetchJoin()
         .where(qUser.email.eq(email));
     return Optional.ofNullable(query.fetchOne());
+  }
+
+  @Override
+  public List<User> getRecommend(User user, Integer size) {
+
+    var userQuery = queryFactory.selectFrom(qUser)
+        .where(qUser.gender.eq(user.getGender())
+            .and(qUser.isSmoker.eq(user.getIsSmoker())
+                .and(qUser.region.eq(user.getRegion())
+                    .and(qUser.activityTime.eq(user.getActivityTime())
+                        .and(qUser.minAge.eq(user.getMinAge())
+                            .and(qUser.maxAge.eq(user.getMaxAge())
+                                .and(qUser.id.ne(user.getId()))))))))
+        .orderBy(Expressions.numberTemplate(Double.class, "function('rand')").asc())
+        .limit(size);
+
+    return userQuery.fetch();
   }
 
 }

d80fe83f28973775af28f56222c1b1a0d9ced293
yjjjwww <yjjjwww@naver.com>
2023-06-17 02:33:00 +0900

    refactor : querydsl where 조건 and 여러개 적용 코드 리팩토링

--- src/main/java/com/fab/banggabgo/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/fab/banggabgo/repository/impl/UserRepositoryImpl.java
@@ -11,13 +11,15 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
+
   private final JPAQueryFactory queryFactory;
   QUser qUser = QUser.user;
-  public Optional<User> findByEmail(String email){
-    var query=queryFactory.selectFrom(qUser)
-            .leftJoin(qUser.roles).fetchJoin()
-            .leftJoin(qUser.tag).fetchJoin()
-            .leftJoin(qUser.tag).fetchJoin()
+
+  public Optional<User> findByEmail(String email) {
+    var query = queryFactory.selectFrom(qUser)
+        .leftJoin(qUser.roles).fetchJoin()
+        .leftJoin(qUser.tag).fetchJoin()
+        .leftJoin(qUser.tag).fetchJoin()
         .where(qUser.email.eq(email));
     return Optional.ofNullable(query.fetchOne());
   }
@@ -27,12 +29,12 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 
     var userQuery = queryFactory.selectFrom(qUser)
         .where(qUser.gender.eq(user.getGender())
-            .and(qUser.isSmoker.eq(user.getIsSmoker())
-                .and(qUser.region.eq(user.getRegion())
-                    .and(qUser.activityTime.eq(user.getActivityTime())
-                        .and(qUser.minAge.eq(user.getMinAge())
-                            .and(qUser.maxAge.eq(user.getMaxAge())
-                                .and(qUser.id.ne(user.getId()))))))))
+            .and(qUser.isSmoker.eq(user.getIsSmoker()))
+            .and(qUser.region.eq(user.getRegion()))
+            .and(qUser.activityTime.eq(user.getActivityTime()))
+            .and(qUser.minAge.eq(user.getMinAge()))
+            .and(qUser.maxAge.eq(user.getMaxAge()))
+            .and(qUser.id.ne(user.getId())))
         .orderBy(Expressions.numberTemplate(Double.class, "function('rand')").asc())
         .limit(size);
 

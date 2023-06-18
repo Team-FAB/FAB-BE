2aa5d5304602914157dbb8f59e6b8cb1ddb0aea7
yjjjwww <yjjjwww@naver.com>
2023-06-18 22:33:22 +0900

    feat : 유저 프로필 정보 불러오기 기능 구현

--- src/main/java/com/fab/banggabgo/service/UserService.java
+++ src/main/java/com/fab/banggabgo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.fab.banggabgo.service;
 
+import com.fab.banggabgo.dto.user.ProfileDto;
 import com.fab.banggabgo.dto.user.RecommendResponseDto;
 import com.fab.banggabgo.entity.User;
 
@@ -11,6 +12,7 @@ public interface UserService {
   RecommendResponseDto getRecommendUsers(User user, Integer size);
 
   /**
-   *
+   * 유저의 프로필 정보 불러오기
    */
+  ProfileDto getUserProfile(int id);
 }

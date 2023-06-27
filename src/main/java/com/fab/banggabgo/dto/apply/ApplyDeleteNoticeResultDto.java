960a2722bb3e7b24e6fcf3e812765677f97561da
sjdlr <sjdlrnen0507@naver.com>
2023-06-27 22:04:58 +0900

    fix: 알림을 읽음으로 표시하는 dto

---
+++ src/main/java/com/fab/banggabgo/dto/apply/ApplyDeleteNoticeResultDto.java
@@ -0,0 +1,17 @@
+package com.fab.banggabgo.dto.apply;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApplyDeleteNoticeResultDto {
+
+  Long applyId;
+}

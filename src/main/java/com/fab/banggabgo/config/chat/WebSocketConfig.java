62a893d2bceeb9afdcf78db77955237dcc132a04
sjdlr <sjdlrnen0507@naver.com>
2023-06-30 21:25:03 +0900

    refactor: ws 에서 wss 로 통신방식 변경

--- src/main/java/com/fab/banggabgo/config/chat/WebSocketConfig.java
+++ src/main/java/com/fab/banggabgo/config/chat/WebSocketConfig.java
@@ -40,10 +40,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry
-        .addEndpoint("/ws")
+        .addEndpoint("/wss")
         .setAllowedOriginPatterns("*");
     registry
-        .addEndpoint("/ws")
+        .addEndpoint("/wss")
         .setAllowedOriginPatterns("*")
         .withSockJS();
   }
